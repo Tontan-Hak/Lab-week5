4d079e11a530139d7e923564e60c4ee5ae76c8de
Tontan-Hak <tontanhak97@gmail.com>
2022-07-21 11:18:38 +0700

    Add new lesson of Senteces or Lines

---
+++ Python/Lesson5.py
@@ -0,0 +1,9 @@
+# Sentences or Lines
+x = 2  # Assignment statement
+x = x + 2  # Assignment with expression
+print(x)   # Print statement
+
+# x called Variable
+# 2 Called constant 
+# + Called Operator
+# print() called Function
