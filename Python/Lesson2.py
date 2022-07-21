982fc4eb3c1e4b9c87c6d62dc24ffa33a9feab96
Tontan-Hak <tontanhak97@gmail.com>
2022-07-21 10:35:43 +0700

    Add lesson 2

--- Python/Lesson2.py
+++ Python/Lesson2.py
@@ -2,3 +2,8 @@
 name = input("Please enter your name: ")
 # Display the welcome message 
 print("Welcome", name, "!")
+
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+total = n1 + n2 
+print("The total is : ", total)
---
+++ Python/Lessson3.py
@@ -0,0 +1,6 @@
+# Adding Numbers
+s = input("Enter a string : ")
+n = int(input("Enter an integer: "))
+x = float(input("Enter a real number: "))
+print()
+print("Inputs recieved: ", s, n, x)
