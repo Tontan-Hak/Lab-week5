077b88aa7afeb2a7fac8aee3d24bc59ec7ebf80c
Tontan Hak <tontanhak97@gmail.com>
2024-04-25 10:38:13 +0700

    debug code in arrays

---
+++ C++/test3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myNumbers[] = {1, 2, 3, 4, 5};
+    cout << myNumbers[0] << endl;
+    cout << myNumbers[1] << endl;
+    cout << myNumbers[2] << endl;
+    cout << myNumbers[3] << endl;
+    cout << myNumbers[4] << endl;
+
+    return 0;
+}
+// int myNumbers[] = {1, 2, 3, 4, 5};
