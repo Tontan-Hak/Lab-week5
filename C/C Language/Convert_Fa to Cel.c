ff2d7bbf4b115bf686057e93d666c4b906ace5ac
Tontan Hak <tontanhak97@gmail.com>
2024-04-26 15:24:33 +0700

    Function declaration

--- C/C Language/Convert_Fa to Cel.c
+++ C/C Language/Convert_Fa to Cel.c
@@ -11,3 +11,20 @@ int main(){
 
     return 0;
 }
+
+/*#include <stdio.h>
+
+// Function declaration
+int myFunction(int, int);
+
+// The main method
+int main() {  
+  int result = myFunction(5, 3);  // call the function
+    printf("Result is = %d", result);
+    return 0;
+}
+
+// Function definition
+    int myFunction(int x, int y) {
+        return x + y;
+}*/
