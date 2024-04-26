300f5670b0824f7998d9af7c42ca952a7b6e4590
Tontan Hak <tontanhak97@gmail.com>
2024-04-26 13:52:51 +0700

    Function

---
+++ C/C Language/Function.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void myFunction(){
+    printf("This is my function\n");
+}
+int main(){
+    myFunction();
+    return 0;
+}
---
+++ C/C Language/parameter.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* void myFunction(char *name, int age){
+    printf("My name is %s and I am %d years old\n", name, age);
+}
+
+int main(){
+    myFunction("John",50);
+    myFunction("Doe", 30);
+    myFunction("Jane", 25);
+
+    return 0; 
+} */
+
+void myFunction(int myNumber[5]){
+    for (int i = 0; i < 5; i++){
+        printf("%d\n", myNumber[i]);
+    }
+}
+
+int main(){
+    int myNumber[5] = {12,24,36,48,52};
+    myFunction(myNumber);
+    return 0;
+}
