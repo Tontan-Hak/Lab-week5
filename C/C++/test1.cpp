6c9e0e27d8987ec828f71193dee8c44212a79219
Tontan Hak <tontanhak97@gmail.com>
2024-04-27 11:54:24 +0700

    output of Cpp code testing and start learning

--- C/C++/test1.cpp
+++ C/C++/test1.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 int main(){
     cout << "Code like a Pro!";
+    return 0;
+} */
+
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!\t \n\n" ; 
+    std::cout << "I am learning C++\\" << std::endl ;
+    std::cout << "I want to be \"Software Engineer\".";
     return 0;
 }
