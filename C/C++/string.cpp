fdeec7b82c737978284fab1df7c5c8e822ca1d87
Tontan Hak <tontanhak97@gmail.com>
2024-04-29 23:51:35 +0700

    String code testing code

---
+++ C/C++/string.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string greeting = "Hello ";
+    string code = "C++"; 
+    cout << greeting.append(code);
+    return 0;
+}
+
+// String Length
+/*
+int main() {
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is: " << txt.length(); // Or you can use txt.size();
+    return 0;
+}*/
+
+// Assess String 
+/*
+int main() {
+    string myString = "Hello";
+    cout << myString[0];
+    // myString[0] = 'J'; // Change the character
+    return 0;
+}*/
