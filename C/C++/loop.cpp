ab5d8e01df3846b0949139cc83b80a05af2165dd
Tontan Hak <tontanhak97@gmail.com>
2024-05-02 11:28:40 +0700

    while loop for Cpp

---
+++ C/C++/loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+
+/* int main(){
+    int i = 0;
+    while (i<5){
+        cout << i <<"\n";
+        i++;
+    }
+    return 0;
+} */
+
+int main() {
+    int i = 0;
+    do {
+        cout << i << "\n";
+        i++;
+    }
+    while (i < 5);
+    return 0; 
+}
