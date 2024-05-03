290b1988b812ba1a570410b408c3fc3ba8f6a31c
Tontan Hak <tontanhak97@gmail.com>
2024-05-03 09:57:33 +0700

    Function

---
+++ C/C++/function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void myFunction(){
+    cout << "I just got executed!";
+}
+
+int main(){
+    myFunction(); // Can called multiple function
+    //myFunction();
+    //myFunction();
+    return 0;
+}
