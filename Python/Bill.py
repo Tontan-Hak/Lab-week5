1f3f3a4bce56890c3f428639d34eefa06c890bab
Tontan Hak <tontanhak97@gmail.com>
2024-08-14 13:06:10 +0700

    add module2 files and create new

--- Python/Bill.py
+++
@@ -1,8 +0,0 @@
-# Each person needs to pay:
-
-bill = 47.28 # Assign "bill" variable with bill amount
-tip = bill * 0.15 # Multiply by stated tip rate 
-total = bill + tip # Sum the "total" of the "bill" and "tip"
-share = total / 2 # Divide "total" by number of friends dining
-print("Each person needs to pay: $" + str(round(share, 2))) # Enter the required string and "share" 
-# Hint: Remember to convert incompatible data types
--- Python/Func.py
+++
@@ -1,8 +0,0 @@
-# Function
-
-def greeting(name, department):
-    print("Welcome, " + name)
-    print("You are part of " + department)
-    
-greeting("Blake", "Software engineering")
-greeting("Ellis", "Software engineering")
