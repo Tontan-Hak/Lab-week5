cfb0f72df0b7da9b7a326181e9a676494521352e
Tontan Hak <tontanhak97@gmail.com>
2024-08-21 10:27:05 +0700

    add info-person code test in c

---
+++ C/C Language/Info-Person.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+    printf("Hello Crush...!\n ");
+    printf("Hello Crush...!\n");
+
+    printf("\n\n");
+
+    printf(" :------------------->[INFORMATION PERSON]<--------------------|\n");
+    printf(" :ID                :ETECOO1                                   |\n");
+    printf(" :Name              :Kimhor                                    |\n");
+    printf(" :Gender            :Male                                      |\n");
+    printf(" :Age               :20                                        |\n");
+    printf(" :Date of Birth     :01/07/2024                                |\n");
+    printf(" :Address           :Phnom Penh                                |\n");
+    printf(" :Salary            :500                                       |\n");
+
+    printf(" :-------------------------------------------------------------\n");
+
+    printf("\n\n");
+    printf("\t\t\t Example 2\n");
+    printf(" :-------------------------------------------------------------\n");
+    printf(" :ID      Name      Gender      AGE     DOB         ADDRESS    \n");
+    printf(" :ETEC101 Lika      Female      20      23/12/24    Phnom Penh \n");
+    printf(" :ETEC102 Tontan    Male        20      23/10/24    Phnom Penh \n");
+    printf(" :ETEC103 Minh      Male        20      23/02/24    Phnom Penh \n");
+    printf("\n\n");
+
+    return 0;
+}
