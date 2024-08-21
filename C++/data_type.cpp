f8e7b37f2ee2e3ec3c88532a10064a0cf54a0ead
Tontan Hak <tontanhak97@gmail.com>
2024-08-21 11:13:39 +0700

    edit code

---
+++ C++/data_type.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int c1 =23;
+    float f1 = 35e3;
+    double d1 = 12E4;
+    bool b1 = true;
+    char k1 = 'T';
+    string s1 = "Tontan";
+
+    cout << c1 << "\n";
+    cout << f1 << "\n";
+    cout << d1 << "\n";
+    cout << b1 << "\n";
+    cout << k1 << "\n";
+    cout << s1;
+    return 0;
+}
