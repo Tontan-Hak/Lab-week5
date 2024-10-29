79dd3fc81105215da414ea16463a2c0d625d0556
Tontan Hak <tontanhak97@gmail.com>
2024-10-29 22:37:11 +0700

    Escape Sequences

---
+++ C/Escape_Sequences.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Step 1: Use escape sequences to print the special characters
+    // Example output:
+    // She said, "C is fun!"
+    // The file path is: C:\ProgramFiles\C
+    printf("She said, \"C is fun!\"\n");
+    printf("The file path is: C:\\ProgramFiles\\C\n");
+    return 0;
+}
